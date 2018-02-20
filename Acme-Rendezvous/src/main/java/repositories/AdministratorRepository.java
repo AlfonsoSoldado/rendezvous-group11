f00ef261fc9cad2217a5ba8282fd75297c2145df
José Manuel Rojas Granado <jrojasgranado@gmail.com>
2018-02-20 17:19:11 +0100

    admin

--- Acme-Rendezvous/src/main/java/repositories/AdministratorRepository.java
+++ Acme-Rendezvous/src/main/java/repositories/AdministratorRepository.java
@@ -1,6 +1,7 @@
 package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Administrator;
@@ -9,8 +10,16 @@ import domain.Administrator;
 public interface AdministratorRepository extends JpaRepository<Administrator, Integer> {
 
 	
+	@Query("select avg(m.rendezvous.size) from User m")
+	int averageRedezvousUser();
+	
+	@Query("select  stddev(m.rendezvous.size) from User m")
+	int EstandardDesviationRedezvousUser();
 
+	@Query("select (select count(a) from Application a where a.status='PENDING')/count(ap)*1.0 from Application ap")
+	int ratioCreateAndNoCreateRendezvousUser();
 
+	select (select count(a) from User a where a.rendezvous.size>0)/count(ap)*1.0 from User ap union (select count(b) from User b where b.rendezvous.size=0)/count(c)*1.0 from User c;	
 	// Simple CRUD methods ----------------------------------------------------
 
 }
