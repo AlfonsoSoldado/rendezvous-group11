7d49ba77fcce45adfdad420ae0ab950ee56e1d86
José Manuel Rojas Granado <jrojasgranado@gmail.com>
2018-02-21 19:30:28 +0100

    admin

--- Acme-Rendezvous/src/main/java/repositories/AdministratorRepository.java
+++ Acme-Rendezvous/src/main/java/repositories/AdministratorRepository.java
@@ -14,10 +14,10 @@ import domain.Rendezvous;
 public interface AdministratorRepository extends JpaRepository<Administrator, Integer> {
 
 	
-	@Query("select avg(m.rendezvous.size) from User m")
+	@Query("select avg(m.rendezvous.size*1.0) from User m")
 	double averageRedezvousUser();
 	
-	@Query("select  stddev(m.rendezvous.size) from User m")
+	@Query("select  stddev(m.rendezvous.size*1.0) from User m")
 	double EstandardDesviationRedezvousUser();
 
 	@Query("select (select count(a) from User a where a.rendezvous.size>=1)/count(ap)*1.0 from User ap;")
@@ -31,54 +31,63 @@ public interface AdministratorRepository extends JpaRepository<Administrator, In
 	double ratioUserSinRendezvous();
 	
 	
-	@Query("select avg(m.attendant.size) from Rendezvous m")
+	@Query("select avg(m.attendant.size*1.0) from Rendezvous m")
 	double averageUsersRendezvous();
 
-	@Query("select  stddev(m.attendant.size) from Rendezvous m")
+	@Query("select stddev(m.attendant.size*1.0) from Rendezvous m")
 	double estandardDesviationUsersRendezvous();
 	
-	@Query("select avg(m.rsvp.size) from User m join m.rsvp e where e.confirmed=true")
+	@Query("select avg(m.rsvp.size*1.0) from User m join m.rsvp e where e.confirmed=true")
 	double averageRendezvousRSVPTruePerUser();
 	
-	@Query("select stddev(m.rsvp.size) from User m join m.rsvp e where e.confirmed=true")
+	@Query("select stddev(m.rsvp.size*1.0) from User m join m.rsvp e where e.confirmed=true")
 	double estandardDesviationRendezvousRSVPTruePerUser();
 	
 	
 	@Query("select m.rendezvous from User m join m.rsvp e where e.confirmed=true order by m.rsvp.size")
 	Collection<Rendezvous> topRendezvous(Pageable a);
 	
-	//revisar
-	@Query("select count(e.announcement)*1.0/(select count(r) from Rendezvous r)*1.0 from User m join m.rendezvous e where e.announcement!=null")
-	double averageannouncementsUser();
+	@Query("select avg(e.announcement.size*1.0) from Rendezvous e")
+	double averageAnnouncementsRendezvous();
+	
+	@Query("select stddev(e.announcement.size*1.0) from Rendezvous e")
+	double estandardDesviationAnnouncementsUser();
+	
 	
 	@Query("select m from Rendezvous m where m.similar.size > (select avg(v.similar.size)*1.1 from Rendezvous v)")
 	double redezvousSimiliars10();
 	
-	@Query("select avg(m.question.size) from Rendezvous m")
+	//revisar
+	@Query(" select ((count(m)*1.0)/(select count(h) from Rendezvous h))from Question m where m.rendezvous.id in (select r.id from Rendezvous r)")
 	double averageNumberOfQuestionsPerRendezvous();
-	
-	@Query("select stddev(m.question.size) from Rendezvous m")
+	//revisar
+
+	@Query("select stddev(m.question.size*1.0) from Rendezvous m")
 	double estandardDesviationOfQuestionsPerRendezvous();
 	
 	//revisar
 	@Query("select count(m.answer.size)/(select count(f) from Rendezvous f)*1.0 from Question m")
 	double averageOfAnswerPerQuestionsPerRendezvous();
-	
-	@Query("select stddev((m.answer.size)/(select count(f) from Rendezvous f)*1.0) from Question m")
+	//revisar
+	@Query("select stddev((m.answer.size*1.0)/(select count(f) from Rendezvous f)*1.0) from Question m")
 	double estandardDesviationOfAnswerPerQuestionsPerRendezvous();
+	
+	
+	
+	
+	@Query("select r from Rendezvous r where r.announcement.size >(select avg(n.announcement.size)*0.75 from Rendezvous n)")
+	Collection<Rendezvous> RendezvousConMas075Announcement();
 	
 	/*
 	 * preguntar a muller si es más eficiente sacar la media y la desviacion en una sola consulta o en varias
 	 * 
 	 * The average and the standard deviation of announcements per rendezvous. revisar
- The rendezvouses that whose number of announcements is above 75% the average number of announcements per rendezvous. revisar
 
- T
+
 
  The average and the standard deviation of the number of answers to the questions per rendezvous.
  The average and the standard deviation of replies per comment
 	 * */
-	
 	
 }
 
